2628b68e78583cbaa035400a5eb25f356542bb26
Syed mozammil karim <47852028+mozammilkarim@users.noreply.github.com>
2019-10-03 11:18:32 +0530

    Update funcStringConcatenation.cpp

--- funcStringConcatenation.cpp
+++ funcStringConcatenation.cpp
@@ -1,6 +1,8 @@
 void strcat(char *a,char *b){
 int i=0,j=0;
 char *t = a;
+/*here important thing is accessing strings without any integer*/
+/*i have copied this from  github.com/prateek27/Coding-Problems*/
 while(*t!='\0'){
 t++;
 }
@@ -8,7 +10,8 @@ t++;
 char *t2 = b;
 
 while(*t2!='\0')
-{ *t = *t2 ;
+{ 
+  *t = *t2 ;//important step
   t++;
   t2++;
 }
